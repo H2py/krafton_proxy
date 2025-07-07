1b26c4edf8d280a10630e2a02a1953882f148e49
H2py <ahdosj@gmail.com>
2025-07-07 11:34:17 +0900

    Now tiny opens as soon as possible

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -14,8 +14,7 @@ int parse_uri(char *uri, char *filename, char *cgiargs);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main(int argc, char **argv) {
   int listenfd, connfd;
@@ -48,7 +47,7 @@ int main(int argc, char **argv) {
 void doit(int fd)
 {
   int is_static; // bool type처럼 사용할 int is_static
-  struct stat sbuf; // sbuf를 stat 구조체로 선언한 이유는 무엇일까?
+  struct stat sbuf; // 파일의 메타데이터를 담는 sbuf 생성
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE]; // 필요한 인자들 선언
   char filename[MAXLINE], cgiargs[MAXLINE]; // cgiargs란.. 무엇일까..?
   rio_t rio; // 내부 버퍼를 사용하는 rio 생성
@@ -70,7 +69,8 @@ void doit(int fd)
     return;
   }
   
-  if (is_static) { // 이후, stat 구조체가 어떤 역할을 하는지, S_ISREG와 같은 플래그가 어떤 역할을 하는지 몰라서 막혔음
+  if (is_static) { // 이후, stat 정적 파일일 때
+    // 정규 파일이 아니고, 유저가 다룰 수 없다면 에러를 내뱉음, 그렇지 않으면 serve_static 정적 파일을 클라이언트에게 전달한다
     if(!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
     }
@@ -120,29 +120,121 @@ void read_requesthdrs(rio_t *rp)
   return;
 }
 
+// http://example.com/cgi-bin/adder?arg1=1&arg2=2와 같이 생긴 문자열
+// cgi가 포함되어 있지 않다면, ./uri 형태로 만듦, 루트면 ./home.html cgi-bin이 있으면 쿼리 스트링 저장한다
 int parse_uri(char *uri, char *filename, char *cgiargs)
 {
   char *ptr;
 
-  // text = "hello world"일 때, ststr(text, "world");는 text내에서 "특정 문자열이 처음으로 나오는 위치를 찾는다"
-  if(!strstr(uri, "cgi-bin")) {
-    strcpy(cgiargs, "");
+  if(!strstr(uri, "cgi-bin")) { // 정적콘텐츠이면, cgiargs의 인자 공백으로 만듦
+    strcpy(cgiargs, ""); // 현재 디렉토리에 uri를 더한다
     strcpy(filename, ".");
-    strcat(filename, uri); 
-    if (uri[strlen(uri) - 1] == '/')
+    strcat(filename, uri);
+    if(uri[strlen(uri) - 1] == '/')
       strcat(filename, "home.html");
     return 1;
   }
   else {
-    ptr = index(uri, '?');
+    ptr = index(uri, '?'); // 포인터가 NULL이 아니면, cgiargs에, arg1=1&arg2=2를 저장한다, ?를 NULL처리 한다
     if (ptr) {
       strcpy(cgiargs, ptr+1);
       *ptr = '\0';
     }
-    else
-      strcpy(cgiargs, ""); // strcat(char *dest, const char *src) dest 뒤에다가 src
+    else 
+      strcpy(cgiargs, ""); // 없으면 공백을 담는다
+
     strcpy(filename, ".");
     strcat(filename, uri);
     return 0;
   }
 }
+
+void serve_static(int fd, char *filename, int filesize) // 클라이언트와 연결되어 있는 파일 디스크립터, 전송할 파일 경로 및 크기 인자로 받음
+{
+  int srcfd;
+  char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+  get_filetype(filename, filetype);
+  sprintf(buf, "HTTP/1.0 200 OK \r\n");
+  sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+  sprintf(buf, "%sConnection: close\r\n", buf);
+  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+  Rio_writen(fd, buf, strlen(buf));
+  printf("Response headers: \n");
+  printf("%s", buf);
+
+  srcfd = Open(filename, O_RDONLY, 0); // 요청한 파일 읽기 전용으로 연다
+  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0); // 파일을 메모리에 매핑한다. -> 파일 내용을 한 번에 네트워크로 전송 가능, 데이터 변경 내용 공유 x 
+  Close(srcfd);
+  Rio_writen(fd, srcp, filesize);
+  Munmap(srcp, filesize);
+}
+
+/*
+ * mmap 사용법 : 파일을 메모리에 매핑하여, 파일 내용을 포인터로 메모리처럼 접근이 가능함
+ * mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+ * addr : 0 (커널이 알아서 주소 정해줌), length : 매핑할 크기, prot : 메모리 보호 -> PROT_READ, flags : 매핑 방식 (MAP_PRIVATE : 쓰기 변경 시 복사 됨), fd 매핑 할 파일 디스크립터, offset : 파일의 시작 위치
+ */
+
+// filename이 해당하는 내용 .html .gif. png 등등을 찾아서 text/html, image/png와 같이 바꿔준다
+void get_filetype(char *filename, char *filetype)
+{
+  if (strstr(filename, ".html"))
+    strcpy(filetype, "text/html");
+  else if (strstr(filename, ".gif"))
+    strcpy(filetype, "image/gif");
+  else if (strstr(filename, ".png"))
+    strcpy(filetype, "image/png");
+  else if (strstr(filename, ".jpg"))
+    strcpy(filetype, "image/jpeg");
+  else
+    strcpy(filetype, "text/plain");
+}
+
+
+// 클라이언트가 요청한 CGI Program 실행 & 전송, 클라이언트와 연결된 소켓 파일 디스크립터와, cgi program의 경로(filename)을 받음
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
+  char buf[MAXLINE], *emptylist[] = { NULL };
+  
+  // sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  // Rio_writen(fd, buf, strlen(buf));
+  // sprintf(buf, "Server: Tiny Web Server\r\n");
+  // Rio_writen(fd, buf, strlen(buf));
+
+  // System Call 호출 최소화
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  sprintf(buf, "Server: Tiny Web Server\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  
+  if (Fork() == 0) { // 자식 프로세스에
+    setenv("QUERY_STRING", cgiargs, 1); // 마지막 인자 : overwrite on (0이 아닌 경우)
+    Dup2(fd, STDOUT_FILENO); // fd가 가리키는 소켓으로 표준 출력이 바뀐다 -> CGI program의 출력이 클라이언트에게 전송됨
+    Execve(filename, emptylist, environ); // 새 프로그램을 메모리에 올리고 0부터 시작, 현재 프로세스 이미지를 완전히 교체함, 실패시 - 1 반환, 2번째 인자에는 char *const argv[] 새 프로그램에 전달할 인자 배열들이 들어간다, char *const evp[] 또한 환경 변수 배열이다
+  }
+  Wait(NULL); // 자식 프로세스가 종료될때까지 부모 프로세스 블록 시킴 -> NULL은 자식의 종료상태가 필요없지 않다는 의미를 뜻함
+}
+
+
+// Q. wait 사용 이유? A. 부모가 먼저 종료될 수 있으니, wait 함수를 사용한다 
+/*
+  pid_t pid = fork()  
+  if (pid == 0) {
+    sleep(3);
+    printf("Child Done\n");
+  } else {
+    sleep(1);
+    printf("Parent Done\n"); 
+  }
+  이때, 부모가 먼저 종료된 뒤, 자식이 종료됨
+  자식은 종료된 상태값을 부모에게 전달해야하는데, 부모가 종료되어있기에, 커널은 자식의 종료 상태를 전달하지 못함
+  좀비가 많아지면 PID가 고갈되고, 시스템이 불안정해진다
+*/
+
+
+// Q. fork를 사용하지 않고, Execve를 사용하면 어떤 일이 일어나는가?
+/*
+  프로세스 이미지가 통째로 CGI Program으로 바뀐다
+  따라서, CGI 실행을 자식에게 위임하고, 서버는 계속 살아있도록 만든다
+*/
