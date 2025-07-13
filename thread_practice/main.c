21d67a79ec7c6694befee9a962bc79ed1fb1442a
H2py <ahdosj@gmail.com>
2025-07-14 02:49:35 +0900

    practice about mutex and semaphore

---
+++ thread_practice/main.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+void *thread(void *vargp);
+
+volatile long cnt = 0;
+sem_t mutex;
+
+int main(int argc, char **argv)
+{    
+    long niters;
+    pthread_t tid1, tid2;
+    sem_init(&mutex, 0, 1);
+
+    if(argc != 2) {
+        printf("usage : %s <niters>\n", argv[0]);
+        exit(0);
+    }
+    niters = atoi(argv[1]);
+
+    pthread_create(&tid1, NULL, thread, &niters);
+    pthread_create(&tid2, NULL, thread, &niters);
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+    if (cnt != (2 * niters))
+        printf("BOOM! cnt = %ld\n", cnt);
+    else
+        printf("OK cnt=%ld\n", cnt);
+    exit(0);
+}
+
+void *thread(void *vargp)
+{
+    long i, niters = *((long *)vargp);
+    int sum =0;
+    for (i = 0; i < niters; i++)
+    {    
+        sem_wait(&mutex);
+        cnt++;
+        sem_post(&mutex);
+        sum +=cnt;
+    }
+    printf("pid : %lu, %d\n",(unsigned long)pthread_self(),sum);
+    return NULL;
+}
